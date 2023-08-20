dcc937d8beab43683703eb5d9d5fe8d22fdbfcbf
Rayvik014 <rayvik014@gmail.com>
2023-08-20 14:14:10 +0300

    Вывод базы постранично готов, добавлена функция изменения записи

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from sys import exit
+
+#--Настройки отображения записей:
+
 RECORDS_ON_LIST = 7 #Количество записей на одной странице телефонной книги
 CHARS_FOR_ID = 3 #Количество зарезервированных символов для отображения поля ID
 CHARS_FOR_COMPANY = 20 #Количество зарезервированных символов для названия компании
 CHARS_FOR_NAME = 40 #Количество зарезервированных символов для ФИО
+CHARS_FOR_PHONE = 13 #Количество зарезервированных символов для телефона (13 - чтобы вместился заголовок)
+
 
 def get_id():
     """Функция для нахождения уникального ID для записи.
@@ -28,45 +34,6 @@ def get_id():
             return new_id
 
 
-class PhoneRecord:
-    """Класс записи в телефонной книге. Инициалоизирует элементы в нужном виде
-    """
-    organization = ""
-    name = ""
-    surname = ""
-    fname = ""
-    phone_work = ""
-    phone_personal = ""
-
-    def make_record(self):
-        """Функция подготовки данных для записи в файл.
-        Возвращает готовую строку для записи в файл
-        """
-        record = (f"{get_id()},{self.organization},{self.name},{self.surname},"
-                  f"{self.fname},{self.phone_work},{self.phone_personal};\n")
-        return record
-
-    def read_record(self, record):
-        """Функция разбивает записанные в файл строки по переменным
-        Возвращает список
-        """
-        x = 0
-        context = ["" for z in range(7)]
-        for char in record:
-            if char != ",":
-                if char == ";":
-                    break
-                context[x] += char
-            else:
-                x += 1
-        return context
-
-    def combine_record(self, context):
-        record = (f"{context[0].rjust(CHARS_FOR_ID, ' ')} {context[1].center(CHARS_FOR_COMPANY, ' ')} "
-                  f"{(context[3]+' '+context[2]+' '+context[4]).center(CHARS_FOR_NAME, ' ')}"
-                  f"{context[5].center(13, ' ')} {context[6].center(13, ' ')}") #13 - чтобы вмещалась шапка
-        return record
-
 def phone_validation(number: str):
     """Функция валидации вводимого номера телефона.
     Принимает номер телефона в виде строки.
@@ -75,7 +42,7 @@ def phone_validation(number: str):
     message = ""
     valid_flag = True
     if len(number) != 11:
-        message = "Введите номер в формате 7-код-ххххххх - всего 11 цифр"
+        message = "Введите номер в формате 7кодххххххх - всего 11 цифр"
         valid_flag = False
     elif not number.isdigit():
         message = "Номер телефона должен содержать только цифры"
@@ -100,8 +67,93 @@ def text_validation(text: str):
     return valid_flag, message
 
 
+class PhoneRecord:
+    """Класс записи в телефонной книге. Инициалоизирует элементы в нужном виде
+    """
+    organization = ""
+    name = ""
+    surname = ""
+    fname = ""
+    phone_work = ""
+    phone_personal = ""
+
+    def make_record(self, record_id=get_id()):
+        """Функция подготовки данных для записи в файл.
+        Возвращает готовую строку для записи в файл
+        """
+        record = (f"{record_id},{self.organization},{self.name},{self.surname},"
+                  f"{self.fname},{self.phone_work},{self.phone_personal};\n")
+        return record
+
+    def read_record(self, record):
+        """Функция разбивает записанные в файл строки по переменным
+        Возвращает список
+        """
+        x = 0
+        context = ["" for z in range(7)]
+        for char in record:
+            if char != ",":
+                if char == ";":
+                    break
+                context[x] += char
+            else:
+                x += 1
+        return context
+
+    def combine_record(self, context):
+        record = (f"{context[0].rjust(CHARS_FOR_ID, ' ')} {context[1].center(CHARS_FOR_COMPANY, ' ')} "
+                  f"{(context[3]+' '+context[2]+' '+context[4]).center(CHARS_FOR_NAME, ' ')}"
+                  f"{context[5].center(CHARS_FOR_PHONE, ' ')} {context[6].center(CHARS_FOR_PHONE, ' ')}") #13 - чтобы вмещалась шапка
+        return record
+
+
 class PhoneBook:
-    """Телефонноа/ книга с основными функциями для взаимодействия"""
+    """Телефонная книга с основными функциями для взаимодействия"""
+    def editing(self, record):
+        both_valid_flag = False
+        while not both_valid_flag:
+            valid_flag = False
+            while not valid_flag:
+                record.organization = str(input(f'Организация: {record.organization} ')) or record.organization
+                valid_flag, message = text_validation(record.organization)
+                print(message) if not valid_flag else None
+            valid_flag = False
+            while not valid_flag:
+                record.name = str(input(f'Имя: {record.name} ')) or record.name
+                valid_flag, message = text_validation(record.name)
+                print(message) if not valid_flag else None
+            if record.organization != "" or record.name != "":
+                both_valid_flag = True
+            else:
+                print("Оба поля - Имя и Организация - не могут быть пустыми, заполните пожалуйста")
+
+        valid_flag = False
+        while not valid_flag:
+            record.surname = str(input(f'Фамилия: {record.surname} ')) or record.surname
+            valid_flag, message = text_validation(record.surname)
+            print(message) if not valid_flag else None
+        valid_flag = False
+        while not valid_flag:
+            record.fname = str(input(f'Отчество: {record.fname} ')) or record.fname
+            valid_flag, message = text_validation(record.fname)
+            print(message) if not valid_flag else None
+
+        both_valid_flag = False
+        while not both_valid_flag:
+            valid_flag = False
+            while not valid_flag:
+                record.phone_work = str(input(f"Рабочий телефон: {record.phone_work} ")) or record.phone_work
+                valid_flag, message = phone_validation(record.phone_work)
+                print(message) if not valid_flag else None
+            valid_flag = False
+            while not valid_flag:
+                record.phone_personal = str(input(f"Личный телефон: {record.phone_personal} ")) or record.phone_personal
+                valid_flag, message = phone_validation(record.phone_personal)
+                print(message) if not valid_flag else None
+            if record.phone_work != "" or record.phone_personal != "":
+                both_valid_flag = True
+            else:
+                print("Оба поля номера телефона не могут быть пустыми, заполните пожалуйста")
 
     def add_record(self):
         """Добавить запись в книгу.
@@ -109,86 +161,114 @@ class PhoneBook:
         """
         new_record = PhoneRecord()
         print("Создание новой записи:")
-
-        while new_record.organization == "" and new_record.name == "":
-            valid_flag = False
-            while not valid_flag:
-                new_record.organization = str(input('Организация:  '))
-                valid_flag, message = text_validation(new_record.organization)
-                print(message) if not valid_flag else None
-            valid_flag = False
-            while not valid_flag:
-                new_record.name = str(input('Имя:  '))
-                valid_flag, message = text_validation(new_record.name)
-                print(message) if not valid_flag else None
-            if new_record.organization != "" or new_record.name != "":
-                break
-            print("Оба поля - Имя и Организация - не могут быть пустыми, заполните пожалуйста")
-
-        valid_flag = False
-        while not valid_flag:
-            new_record.surname = str(input('Фамилия:  '))
-            valid_flag, message = text_validation(new_record.surname)
-            print(message) if not valid_flag else None
-        valid_flag = False
-        while not valid_flag:
-            new_record.fname = str(input('Отчество:  '))
-            valid_flag, message = text_validation(new_record.fname)
-            print(message) if not valid_flag else None
-
-        while new_record.phone_work == "" and new_record.phone_personal == "":
-            valid_flag = False
-            while not valid_flag:
-                new_record.phone_work = str(input("Рабочий телефон:  "))
-                valid_flag, message = phone_validation(new_record.phone_work)
-                print(message) if not valid_flag else None
-            valid_flag = False
-            while not valid_flag:
-                new_record.phone_personal = str(input("Личный телефон:  "))
-                valid_flag, message = phone_validation(new_record.phone_personal)
-                print(message) if not valid_flag else None
-            if new_record.phone_work != "" or new_record.phone_personal != "":
-                break
-            print("Оба поля номера телефона не могут быть пустыми, заполните пожалуйста")
-
+        self.editing(new_record)
         with open('phonebase.txt', "at") as phonebase:
             phonebase.write(new_record.make_record())
-
         print('\nЗапись добавлена в книгу')
         self.user_menu()
 
-    def edit_record(self):
-        pass
+    def edit_record(self, selected_record):
+        with open('phonebase.txt', 'rt') as phonebase:
+            for line_number, line in enumerate(phonebase):
+                record_id = ""
+                for char in line:
+                    if char.isdigit():
+                        record_id += char
+                    else:
+                        break
+                if record_id == selected_record:
+                    record = PhoneRecord()
+                    record_context = record.read_record(line)
+                    record_line_number = line_number
+                    break
+        record.organization = record_context[1]
+        record.name = record_context[2]
+        record.surname = record_context[3]
+        record.fname = record_context[4]
+        record.phone_work = record_context[5]
+        record.phone_personal = record_context[6]
+        self.editing(record)
+        with open('phonebase.txt', 'rt') as phonebase:
+            lines = phonebase.readlines()
+        with open('phonebase.txt', 'wt') as phonebase:
+            for line_number, line in enumerate(lines):
+                if line_number == record_line_number:
+                    phonebase.write(record.make_record(int(record_context[0])))
+                else:
+                    phonebase.write(line)
+        self.show_phone_book(1)
 
     def find_record(self):
         pass
 
-    def show_phone_book(self):
+    def show_phone_book(self, page_number):
+        have_forw_list_bool, have_back_list_bool, ids_list = self.combine_records_for_view(page_number)
+        users_choice_list = []
+        users_choice = "_"
+        while users_choice.lower() not in users_choice_list:
+            if have_forw_list_bool:
+                users_choice_list.append("x")
+            else:
+                if "x" in users_choice_list:
+                    users_choice_list.remove("x")
+            if have_back_list_bool:
+                users_choice_list.append("z")
+            else:
+                if "z" in users_choice_list:
+                    users_choice_list.remove("z")
+
+            users_choice = input()
+            if users_choice.lower() == "x":
+                if users_choice.lower() in users_choice_list:
+                    page_number += 1
+                    self.show_phone_book(page_number)
+                else:
+                    continue
+            if users_choice.lower() == "z":
+                if users_choice.lower() in users_choice_list:
+                    page_number -= 1
+                    self.show_phone_book(page_number)
+                else:
+                    continue
+            if users_choice.lower() == "q":
+                PhoneBook().user_menu()
+            if users_choice in ids_list:
+                self.edit_record(users_choice)
+
+
+    def combine_records_for_view(self, page_number):
+        """Получает на вход номер стрницы для отображения.
+        Формирует список записей для отображения, форматирует их, выводит шапку и подвал с инструкциями
+        Возвращает BOOL есть ли предыдущая и следующая страницы
+        """
+        naming = ["ID","Организация","ФИО","Рабочий тел", "Личный тел"]
+        shapka = (f"{naming[0].center(CHARS_FOR_ID, ' ')} {naming[1].center(CHARS_FOR_COMPANY, ' ')} "
+                  f"{naming[2].center(CHARS_FOR_NAME, ' ')}"
+                  f"{naming[3].center(CHARS_FOR_PHONE, ' ')} {naming[4].center(CHARS_FOR_PHONE, ' ')}")
+        print(shapka)
+        print("-" * len(shapka))
+        have_back_list = ""
+        have_forw_list = ""
+        ids_list = []
+        have_back_list_bool = False
+        have_forw_list_bool = False
         with (open("phonebase.txt", "rt") as phonebase):
-            page_number = 1
-            naming = ["ID","Организация","ФИО","Рабочий тел", "Личный тел"]
-            shapka = (f"{naming[0].rjust(CHARS_FOR_ID, ' ')} {naming[1].center(CHARS_FOR_COMPANY, ' ')} "
-                      f"{naming[2].center(CHARS_FOR_NAME, ' ')}"
-                      f"{naming[3].center(13, ' ')} {naming[4].center(13, ' ')}")
-                                                                    # 13 - чтобы вмещалась шапка
-            have_back_list = "Z - листать назад"
-            have_forw_list = ""
-            print(shapka)
-            print("-" * len(shapka))
             for line_number, record in enumerate(phonebase):
                 context = PhoneRecord().read_record(record)
-                if line_number < RECORDS_ON_LIST:
+                ids_list.append(context[0])
+                if page_number > 1:
+                    have_back_list = "Z - листать назад"
+                    have_back_list_bool = True
+                if RECORDS_ON_LIST * page_number > line_number >= RECORDS_ON_LIST * (page_number - 1):
                     print(PhoneRecord().combine_record(context))
-                if line_number == RECORDS_ON_LIST:
+                if line_number == RECORDS_ON_LIST * page_number:
                     have_forw_list = "X - листать вперед"
-
-            podval = (f"{'-' * len(shapka)}\nСтр.{page_number}     {have_back_list}      {have_forw_list}\n"
-                      f"Чтобы редактировать запись, введите ее ID")
-            print(podval)
-
-
-
-
+                    have_forw_list_bool = True
+                    break
+        podval = (f"{'-' * len(shapka)}\nСтр.{page_number}     {have_back_list}      {have_forw_list}\n"
+                  f"Чтобы редактировать запись, введите ее ID     Q - Для выхода в меню")
+        print(podval)
+        return have_forw_list_bool, have_back_list_bool, ids_list
 
     def user_menu(self):
         print('\nТЕЛЕФОНАЯ КНИГА\n\n'
@@ -201,14 +281,14 @@ class PhoneBook:
             user_choice = input('Введите номер пункта и нажмите ENTER\n')
             print('Такого пункта нет, попробуйте снова') if user_choice not in {"1", "2", "3", "4"} else None
         if user_choice == "1":
-            self.show_phone_book()
+            self.show_phone_book(page_number=1)
         if user_choice == "2":
             self.add_record()
         if user_choice == "3":
             self.find_record()
         if user_choice == "4":
             print("Вы вышли из программы")
-
+            exit()
 
 if __name__ == '__main__':
     PhoneBook().user_menu()
